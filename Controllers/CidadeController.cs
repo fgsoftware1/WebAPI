173e7b8bc6c79fa1c453f639ea4769692d5f29fd
gabriel aguiar <fgsoftwarestudio@gmail.com>
2022-02-21 11:13:06 +0000

    commit

--- Controllers/CidadeController.cs
+++ Controllers/CidadeController.cs
@@ -22,14 +22,14 @@ namespace WebAPI_prog3.Controllers
         }
 
         // GET: Cidades
-        public async Task<IActionResult> Index()
+        public async Task<List<Cidade>> GetAll_Cidade()
         {
             var projecto_webapiContext = _context.Cidades.Include(c => c.IdPaisNavigation);
-            return View(await projecto_webapiContext.ToListAsync());
+            return await projecto_webapiContext.ToListAsync();
         }
 
         // GET: Cidades/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<object> Details(int? id)
         {
             if (id == null)
             {
@@ -44,119 +44,7 @@ namespace WebAPI_prog3.Controllers
                 return NotFound();
             }
 
-            return View(cidade);
-        }
-
-        // GET: Cidades/Create
-        public IActionResult Create()
-        {
-            ViewData["IdPais"] = new SelectList(_context.Pais, "IdPais", "Moeda");
-            return View();
-        }
-
-        // POST: Cidades/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdCidade,Nome,Regiao,IdPais")] Cidade cidade)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(cidade);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdPais"] = new SelectList(_context.Pais, "IdPais", "Moeda", cidade.IdPais);
-            return View(cidade);
-        }
-
-        // GET: Cidades/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cidade = await _context.Cidades.FindAsync(id);
-            if (cidade == null)
-            {
-                return NotFound();
-            }
-            ViewData["IdPais"] = new SelectList(_context.Pais, "IdPais", "Moeda", cidade.IdPais);
-            return View(cidade);
-        }
-
-        // POST: Cidades/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdCidade,Nome,Regiao,IdPais")] Cidade cidade)
-        {
-            if (id != cidade.IdCidade)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(cidade);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CidadeExists(cidade.IdCidade))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["IdPais"] = new SelectList(_context.Pais, "IdPais", "Moeda", cidade.IdPais);
-            return View(cidade);
-        }
-
-        // GET: Cidades/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var cidade = await _context.Cidades
-                .Include(c => c.IdPaisNavigation)
-                .FirstOrDefaultAsync(m => m.IdCidade == id);
-            if (cidade == null)
-            {
-                return NotFound();
-            }
-
-            return View(cidade);
-        }
-
-        // POST: Cidades/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var cidade = await _context.Cidades.FindAsync(id);
-            _context.Cidades.Remove(cidade);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool CidadeExists(int id)
-        {
-            return _context.Cidades.Any(e => e.IdCidade == id);
+            return cidade;
         }
     }
 }
---
+++ Controllers/EditorasController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebAPI_prog3.Models;
+
+namespace WebAPI_prog3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class EditorasController : Controller
+    {
+        private readonly projecto_webapiContext _context;
+
+        public EditorasController(projecto_webapiContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/Editoras
+        public async Task<List<Editora>> GetAll_Editora()
+        {
+            return await _context.Editoras.ToListAsync();
+        }
+
+        // GET api/Editoras/Details/
+        [HttpGet("Details/{id}")]
+        public async Task<object> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var editora = await _context.Editoras
+                .FirstOrDefaultAsync(m => m.IdEditora == id);
+            if (editora == null)
+            {
+                return NotFound();
+            }
+
+            return editora;
+        }
+
+
+        // GET api/Editoras/Create
+        [HttpPost("Create")]
+        public async Task<StatusCodeResult> Create([FromBody] Editora editora)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(editora);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("Edit")]
+        //PUT api/Editoras/Edit
+        public async Task<StatusCodeResult> Edit([FromBody] Editora editora)
+        {
+            if(editora.IdEditora == 0)
+            {
+                return NotFound();
+            }
+
+            var result = await _context.Editoras.FindAsync(editora.IdEditora);
+
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            result.Nome = editora.Nome;
+            result.Ativo = editora.Ativo;
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(result);
+
+                    await _context.SaveChangesAsync();
+
+                    return Ok();
+                }catch(DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                }
+            }
+
+            return BadRequest();
+        }
+    }
+}
